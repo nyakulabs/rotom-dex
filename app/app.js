142bb9f086752e08c52968a4a7990601c941f620
Nox <aden.lequire@gmail.com>
2018-08-26 18:53:27 -0400

    please

--- app/app.js
+++ app/app.js
@@ -26,11 +26,16 @@ app.setHandler({
     'PokemonInfo': function(pokemon) {
         pokedex.getPokemonSpeciesByName(pokemon.value.toLowerCase())
         .then(response => {
-            let pkmnName = response.names.filter(o => o.language.name === 'en')[0].name;
-            let pkmnSpecies = response.genera.filter(o => o.language.name === 'en')[0].genus;
-            let pkmnDesc = response.flavor_text_entries.filter(o => o.language.name === 'en')[0].flavor_text;
-            let fullResponse = `${pkmnName}, the ${pkmnSpecies}. ${pkmnDesc}`;
-            this.tell(fullResponse);
+            try {
+                let pkmnName = response.names.filter(o => o.language.name === 'en')[0].name;
+                let pkmnSpecies = response.genera.filter(o => o.language.name === 'en')[0].genus;
+                let pkmnDesc = response.flavor_text_entries.filter(o => o.language.name === 'en')[0].flavor_text;
+                let fullResponse = `${pkmnName}, the ${pkmnSpecies}. ${pkmnDesc}`;
+                this.tell(fullResponse);
+            } catch(err) {
+                this.tell('An error has occurred.');
+                console.log(err);
+            }
         })
         .catch(err => {
             console.log(err);
