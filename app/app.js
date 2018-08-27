f8dc2a58c2d6c4ac59ac4a471c5cd1e2f6a58f0b
Nox <aden.lequire@gmail.com>
2018-08-27 19:58:57 -0400

    egwegwehgwehaerh

--- app/app.js
+++ app/app.js
@@ -36,8 +36,7 @@ app.setHandler({
         console.log('New client.');
     },
 
-    'PokemonInfo': function(pokemon) {
-
+    'PokemonInfoSingle': function(pokemon) {
             try {
                 let pkmnObj = pokemon_species.filter(o => (
                     o.identifier === pokemon.value.toLowerCase()
@@ -70,6 +69,44 @@ app.setHandler({
                 console.log(err);
             }
     },
+
+    'PokemonInfoContinue': function(pokemon) {
+            try {
+                let pkmnObj = pokemon_species.filter(o => (
+                    o.identifier === pokemon.value.toLowerCase()
+                ))[0];
+
+                if (!pkmnObj) {
+                    this.ask('Sorry, I don\'t know about that Pokémon.');
+                } else {
+                    let pkmnID = pkmnObj.id;
+
+                    let pkmnSpNameObj = pokemon_species_names.filter(o => (
+                       o.pokemon_species_id === pkmnID && o.local_language_id === 9
+                    ))[0];
+
+                    let pkmnName = pkmnSpNameObj.name;
+                    let pkmnGenus = pkmnSpNameObj.genus;
+
+                    let pkmnFlavorObj = pokemon_species_flavor_text.filter(o => (
+                        o.language_id === 9 && o.species_id === pkmnID
+                    ));
+
+                    let pkmnFlavor = pkmnFlavorObj[pkmnFlavorObj.length - 1].flavor_text.replace(/[\r\n]+/g,' ');
+
+                    let fullResponse = `${pkmnName}, the ${pkmnGenus}. ${pkmnFlavor}`;
+                    this.ask(fullResponse);
+                    console.log(`Request for ${pkmnName} fulfilled. [${Date.now()}]`);
+                }
+            } catch(err) {
+                this.ask('An error has occurred.');
+                console.log(err);
+            }
+    },
+
+    'Goodbye': function() {
+        this.tell('Goodbye!');
+    }
 });
 
 module.exports.app = app;
