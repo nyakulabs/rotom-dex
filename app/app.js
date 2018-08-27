1022a12854c2eb12deb6f2a34d8742fbf7ab0a34
Nox <aden.lequire@gmail.com>
2018-08-27 18:39:48 -0400

    please work

--- app/app.js
+++ app/app.js
@@ -42,23 +42,23 @@ app.setHandler({
                 let pkmnObj = pokemon_species.filter(o => (
                     o.identifier === pokemon.value.toLowerCase()
                 ));
-                console.log(pkmnObj)[0];
-                let pkmnID = pkmnObj[0].id;
-                console.log(pkmnID[0]);
+                console.log(pkmnObj);
+                let pkmnID = pkmnObj.id;
+                console.log(pkmnID);
                 let pkmnSpNameObj = pokemon_species_names.filter(o => (
                    o.pokemon_species_id === pkmnID && o.local_language_id === 9
-                ));
-                console.log(pkmnSpNameObj)[0];
-                let pkmnName = pkmnSpNameObj[0].name;
-                let pkmnGenus = pkmnSpNameObj[0].genus;
+                ))[0];
+                console.log(pkmnSpNameObj);
+                let pkmnName = pkmnSpNameObj.name;
+                let pkmnGenus = pkmnSpNameObj.genus;
                 console.log(pkmnName);
                 console.log(pkmnGenus);
                 let pkmnFlavorObj = pokemon_species_flavor_text.filter(o => (
                     o.version_id === 29 && o.language_id === 9
-                ));
-                console.log(pkmnFlavorObj[0]);
-                let pkmnFlavor = pkmnFlavorObj[0].flavor_text;
-                console.log(pkmnFlavor[0]);
+                ))[0];
+                console.log(pkmnFlavorObj);
+                let pkmnFlavor = pkmnFlavorObj.flavor_text;
+                console.log(pkmnFlavor);
                 let fullResponse = `${pkmnName}, the ${pkmnGenus}. ${pkmnFlavor}`;
                 this.tell(fullResponse);
                 console.log(`Request for ${pkmnName} fulfilled. [${Date.now()}]`);
