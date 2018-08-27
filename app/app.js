93b0259be9a1341398c7b59fa8f5b378421e60c1
Nox <aden.lequire@gmail.com>
2018-08-27 18:07:00 -0400

    iasubeg9uhs98eh9gsh98egh09sg0

--- app/app.js
+++ app/app.js
@@ -38,21 +38,6 @@ app.setHandler({
     },
 
     'PokemonInfo': function(pokemon) {
-        pokedex.getPokemonSpeciesByName(pokemon.value.toLowerCase())
-        .then(response => {
-            /*
-            try {
-                let pkmnName = response.names.filter(o => o.language.name === 'en')[0].name;
-                let pkmnSpecies = response.genera.filter(o => o.language.name === 'en')[0].genus;
-                let pkmnDesc = response.flavor_text_entries.filter(o => o.language.name === 'en')[0].flavor_text;
-                let fullResponse = `${pkmnName}, the ${pkmnSpecies}. ${pkmnDesc}`;
-                this.tell(fullResponse);
-                console.log(`Request for ${pkmnName} fulfilled. [${Date.now()}]`);
-            } catch(err) {
-                this.tell('An error has occurred.');
-                console.log(err);
-            }
-            */
 
             try {
                 let pkmnObj = json.pokemon_species.filter(o => {
@@ -78,11 +63,6 @@ app.setHandler({
                 this.tell('An error has occurred.');
                 console.log(err);
             }
-        })
-        .catch(err => {
-            console.log(err);
-            this.tell("Sorry, I don't know about that Pokémon.")
-        });
     },
 });
 
