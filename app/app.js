c7f34c70e17f39c35824f61c802f0ca14bf8fdeb
Daniel Baerwalde <2660574+shirtjs@users.noreply.github.com>
2018-08-27 20:18:49 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -36,7 +36,7 @@ app.setHandler({
         console.log('New client.');
     },
 
-    'PokemonInfoSingle': function(pokemon) {
+    'PokemonInfo': function(pokemon) {
             try {
                 let pkmnObj = pokemon_species.filter(o => (
                     o.identifier === pokemon.value.toLowerCase()
@@ -61,45 +61,11 @@ app.setHandler({
                     let pkmnFlavor = pkmnFlavorObj[pkmnFlavorObj.length - 1].flavor_text.replace(/[\r\n]+/g,' ');
 
                     let fullResponse = `${pkmnName}, the ${pkmnGenus}. ${pkmnFlavor}`;
-                    this.tell(fullResponse);
-                    console.log(`Request for ${pkmnName} fulfilled. [${Date.now()}]`);
-                }
-            } catch(err) {
-                this.tell('An error has occurred.');
-                console.log(err);
-            }
-    },
-
-    'PokemonInfoContinue': function(pokemon) {
-            try {
-                let pkmnObj = pokemon_species.filter(o => (
-                    o.identifier === pokemon.value.toLowerCase()
-                ))[0];
-
-                if (!pkmnObj) {
-                    this.ask('Sorry, I don\'t know about that Pokémon.');
-                } else {
-                    let pkmnID = pkmnObj.id;
-
-                    let pkmnSpNameObj = pokemon_species_names.filter(o => (
-                       o.pokemon_species_id === pkmnID && o.local_language_id === 9
-                    ))[0];
-
-                    let pkmnName = pkmnSpNameObj.name;
-                    let pkmnGenus = pkmnSpNameObj.genus;
-
-                    let pkmnFlavorObj = pokemon_species_flavor_text.filter(o => (
-                        o.language_id === 9 && o.species_id === pkmnID
-                    ));
-
-                    let pkmnFlavor = pkmnFlavorObj[pkmnFlavorObj.length - 1].flavor_text.replace(/[\r\n]+/g,' ');
-
-                    let fullResponse = `${pkmnName}, the ${pkmnGenus}. ${pkmnFlavor}`;
                     this.ask(fullResponse);
                     console.log(`Request for ${pkmnName} fulfilled. [${Date.now()}]`);
                 }
             } catch(err) {
-                this.ask('An error has occurred.');
+                this.tell('An error has occurred.');
                 console.log(err);
             }
     },
